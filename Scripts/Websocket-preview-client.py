4efd3daba7649f89e6214a6d78c1eead279c36e9
Ayush Chandan <0201csml001@niet.co.in>
2024-01-27 18:52:16 +0530

    Server working - 🥳

--- Scripts/Websocket-preview-client.py
+++ Scripts/Websocket-preview-client.py
@@ -1,13 +1,21 @@
 
-import asyncio
+import pprint as p
+import asyncio, json
 from websockets.sync.client import connect
 
 def hello():
     with connect("ws://localhost:6969") as websocket:
         while True:
+            message = None
             message = websocket.recv()
-            print(message)
+            if message != None:
+                pro = json.loads(message)
+                print(pro['frameNumber'])
             asyncio.wait(1/15)
         
 
-hello()
+if __name__ == "__main__":
+    try:
+        hello()
+    except KeyboardInterrupt:
+        print("LOG::Disconnected from the server")
--- Scripts/mediapipe-detection.py
+++ Scripts/mediapipe-detection.py
@@ -1,22 +1,23 @@
-import json
-import cv2
 import mediapipe as mp
-import time
+import asyncio, websockets, threading, json, cv2, time
+from websockets.server import serve
+from helper import landmark_to_json_per_frame
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-import asyncio, websockets
-from websockets.server import serve
-from helper import landmarks_to_json, landmark_to_json_per_frame
 
-lmrks = []
-timeTaken = []
-async def detectPose(websocket):
+response = None
+stopServer = threading.Event()
+
+def detectPose():
+    global response
+    global stopServer
     try:
-        #cap = cv2.VideoCapture(1)
-        cap = cv2.VideoCapture("C:\dev\Final Year Project\Pose Estimation\Scripts\DemoVids\demo.mp4")
+        cap = cv2.VideoCapture(0)
+        #cap = cv2.VideoCapture('C:\dev\Final Year Project\MediaPipe Pose Estimation\Scripts\DemoVids\demo.mp4') #- for demosS
         with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
             i = 0
             while cap.isOpened():
+                
                 ret, frame = cap.read()
 
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -27,48 +28,66 @@ async def detectPose(websocket):
                 endTime = (time.time() * 1000)
                 
                 timeTakenForFrame = endTime - startTime
-                timeTaken.append(timeTakenForFrame)
                 
-                print(f"Time taken by frame{i}:", timeTakenForFrame)
-
                 image.flags.writeable = True
                 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
                 try:
-                    response = landmark_to_json_per_frame(results.pose_landmarks.landmark, i)
-                    #await websocket.send(json.dumps(response))
+                    response = json.dumps(landmark_to_json_per_frame(results.pose_landmarks.landmark, i))
+                    print(f"LOG::Frame Detection Success::Time taken by frame{i}:", timeTakenForFrame)
                     i += 1
-                    lmrks.append(results.pose_landmarks.landmark)
                 except:
-                    print("No Person Detected")
-
+                    print("LOG::No Person Detected")
+                
                 mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                           mp_drawing.DrawingSpec(
                                               color=(245, 117, 66), thickness=2, circle_radius=2),
                                           mp_drawing.DrawingSpec(
                                               color=(245, 66, 230), thickness=2, circle_radius=2)
                                           )
-
-                cv2.imshow('Mediapipe Feed', image)
-
-                if cv2.waitKey(10) & 0xFF == ord('q'):
+                
+                cv2.imshow("Mediapipe Window",image)
+                
+                if (cv2.waitKey(10) & 0xFF == ord('q')) or stopServer.is_set():
+                    if stopServer.is_set() != True:
+                        stopServer.set()
                     break
-                await asyncio.wait(1/15)
 
             cap.release()
-            cv2.destroyAllWindows()
+            cv2.destroyAllWindows()            
+            
             
     except Exception as e:
-        print(e)
+        print("LOG::Exception::" + str(e))
         cap.release()
         cv2.destroyAllWindows()
+        
+async def mainServer(websocket, path):
+    global response
+    global stopServer
+    while True:
+        if stopServer.is_set():
+            break
+        if response != None:
+            await websocket.send(response)
+            response = None
+        await asyncio.sleep(1/15)
+    
 
+if "__main__" == __name__:
+    try:
+        stopServer == False
+        detectPoseThread = threading.Thread(target=detectPose)
+        detectPoseThread.start()
+        
+        start_server = serve(mainServer, "localhost", 6969)
 
-async def main():
-    async with serve(detectPose, "localhost", 6969):
-        await asyncio.Future()
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
+    except KeyboardInterrupt:
+        stopServer.set()
+        time.sleep(1)
+        print("LOG::Server has been stopped")
+              
 
-
-
-asyncio.run(main())
     
